d9b122502056dac4120eb5dc7a98284c94367017
Alberto Peinado Santana <albertopeisan@gmail.com>
2024-07-19 23:25:11 +0100

    Add inputs

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -3,6 +3,17 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+  // Get input values
+  const bucket = core.getInput('bucket', { required: true });
+  const region = core.getInput('region', { required: true });
+  const folder = core.getInput('dist-folder', { required: true });
+
+  // Execute a command
+  exec.exec(`echo ${ bucket }, ${ region }, ${ folder }`)
+
+  // You can access github context in the action
+  // github.context.
+
   core.notice('Hello from my custom javascript action')
 }
 
