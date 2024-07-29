605b1296aac786a6dcd2cf022e0c830e4a4339e9
Alberto Peinado Santana <albertopeisan@gmail.com>
2024-07-29 12:27:03 +0100

    Update actions

--- .github/actions/javascript-action/main.js
+++ .github/actions/javascript-action/main.js
@@ -8,12 +8,7 @@ function run() {
   const bucketRegion = core.getInput('bucket-region', { required: true });
   const distFolder = core.getInput('dist-folder', { required: true });
 
-  // 2) Upload files
-  const s3Uri = `s3://${bucket}`;
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
-
-  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
-  core.setOutput('website-url', websiteUrl); // ::set-output
+  exec.exec(`echo folder: ${distFolder}, uri: ${s3Uri}, region: ${bucketRegion}`);
 }
 
 run();
